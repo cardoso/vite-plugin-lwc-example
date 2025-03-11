8c0e2d841023193317b1808a140f9e161a7b983a
Matheus Cardoso <matheus@cardo.so>
2025-03-11 17:26:36 -0300

    update script

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import "@lwc/synthetic-shadow";
 import { createElement } from "lwc";
 import App from "c/app";
 
+document.body.querySelector("c-app")?.remove();
 const elm = createElement("c-app", { is: App });
 document.body.appendChild(elm);
